64322762a67a9704a1e59359ff511ace62fb621b
Larisa Doroshchuk <larisa.doroshchuk@gmail.com>
2023-02-05 14:09:44 +0200

    Updates

---
+++ js/mapdata.js
@@ -0,0 +1,279 @@
+var simplemaps_countrymap_mapdata = {
+  main_settings: {
+    //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#F1F0FF",
+    background_transparent: "yes",
+    border_color: "#665CD1",
+
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: "0.333333",
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: "10",
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "0": {},
+    "1": {},
+    "2": {},
+    "3": {},
+    UKR283: {
+      name: "Crimea",
+      color: "#ebebeb",
+      hover_color: "#ebebeb"
+    },
+    UKR284: {
+      name: "Миколаївська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n30,2 ц./га зібр. площі\nЗібрана площа\n205,9 тис. га\nОбсяги виробництва\n6123,3 тис. ц."
+    },
+    UKR285: {
+      name: "Чернігівська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n30,5 ц./га зібр. площі\nЗібрана площа\n235,9 тис. га\nОбсяги виробництва\n6523,3 тис. ц."
+    },
+    UKR286: {
+      name: "Рівенська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n32,9 ц./га зібр. площі\nЗібрана площа\n211,3 тис. га\nОбсяги виробництва\n5987,2 тис. ц."
+    },
+    UKR288: {
+      name: "Чернівецька область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n31,1 ц./га зібр. площі\nЗібрана площа\n111,2 тис. га\nОбсяги виробництва\n4987,2 тис. ц."
+    },
+    UKR289: {
+      name: "Івано-Франківська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n29,1 ц./га зібр. площі\nЗібрана площа\n181,2 тис. га\nОбсяги виробництва\n3981,4 тис. ц."
+    },
+    UKR290: {
+      name: "Хмельницька область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n34,1 ц./га зібр. площі\nЗібрана площа\n231,7 тис. га\nОбсяги виробництва\n6986,3 тис. ц."
+    },
+    UKR291: {
+      name: "Львівська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n31,6 ц./га зібр. площі\nЗібрана площа\n201,8 тис. га\nОбсяги виробництва\n5698,3 тис. ц."
+    },
+    UKR292: {
+      name: "Тернопільска область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n37,7 ц./га зібр. площі\nЗібрана площа\n289,1 тис. га\nОбсяги виробництва\n6693,1 тис. ц."
+    },
+    UKR293: {
+      name: "Закарпатська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n27,7 ц./га зібр. площі\nЗібрана площа\n183,1 тис. га\nОбсяги виробництва\n3694,5 тис. ц."
+    },
+    UKR318: {
+      name: "Волинська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n34,4 ц./га зібр. площі\nЗібрана площа\n254,2 тис. га\nОбсяги виробництва\n6643,5 тис. ц."
+    },
+    UKR319: {
+      name: "Черкаська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n40,4 ц./га зібр. площі\nЗібрана площа\n352,2 тис. га\nОбсяги виробництва\n8243,5 тис. ц."
+    },
+    UKR320: {
+      name: "Кіровоградська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n37,4 ц./га зібр. площі\nЗібрана площа\n321,1 тис. га\nОбсяги виробництва\n6243,5 тис. ц."
+    },
+    UKR321: {
+      name: "Київська область",
+      color: "#F1F0FF",
+      description: "Урожайність\n31,2 ц./га зібр. площі\nЗібрана площа\n200,9 тис. га\nОбсяги виробництва\n6279,3 тис. ц.",
+      hover_color: "#9d97e2"
+    },
+    UKR322: {
+      name: "Одеська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n27,1 ц./га зібр. площі\nЗібрана площа\n211,1 тис. га\nОбсяги виробництва\n5543,5 тис. ц."
+    },
+    UKR323: {
+      name: "Вінницька область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n39,1 ц./га зібр. площі\nЗібрана площа\n313,1 тис. га\nОбсяги виробництва\n6588,8 тис. ц."
+    },
+    UKR324: {
+      name: "Житомирська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n33,3 ц./га зібр. площі\nЗібрана площа\n300,2 тис. га\nОбсяги виробництва\n6754,2 тис. ц.Вінницька область"
+    },
+    UKR325: {
+      name: "Сумська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n31,3 ц./га зібр. площі\nЗібрана площа\n232,2 тис. га\nОбсяги виробництва\n6254,2 тис. ц."
+    },
+    UKR326: {
+      name: "Дніпропетровська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n30,3 ц./га зібр. площі\nЗібрана площа\n209,2 тис. га\nОбсяги виробництва\n6546,6 тис. ц."
+    },
+    UKR327: {
+      name: "Донецька область",
+      color: "#ebebeb",
+      hover_color: "#ebebeb"
+    },
+    UKR328: {
+      name: "Харківська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n32,3 ц./га зібр. площі\nЗібрана площа\n262,4 тис. га\nОбсяги виробництва\n5324,6 тис. ц."
+    },
+    UKR329: {
+      name: "Луганська область",
+      color: "#ebebeb",
+      hover_color: "#ebebeb"
+    },
+    UKR330: {
+      name: "Полтавська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "Урожайність\n35,3 ц./га зібр. площі\nЗібрана площа\n234,4 тис. га\nОбсяги виробництва\n6126,9 тис. ц."
+    },
+    UKR331: {
+      name: "Запоріжська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "34,3 ц./га зібр. площі\nЗібрана площа\n213,7 тис. га\nОбсяги виробництва\n5436,9 тис. ц."
+    },
+    UKR4826: {
+      name: "м. Київ",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2"
+    },
+    UKR4827: {
+      name: "Херсонська область",
+      color: "#F1F0FF",
+      hover_color: "#9d97e2",
+      description: "27,3 ц./га зібр. площі\nЗібрана площа\n197,7 тис. га\nОбсяги виробництва\n4336,4 тис. ц."
+    },
+    UKR5482: {
+      name: "м. Сувастополь",
+      color: "#ebebeb",
+      hover_color: "#ebebeb"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "50.433333",
+      lng: "30.516667",
+      name: "Kiev"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
+
+if (
+  "IntersectionObserver" in window &&
+  "IntersectionObserverEntry" in window &&
+  "intersectionRatio" in window.IntersectionObserverEntry.prototype
+) {
+  let observer = new IntersectionObserver(entries => {
+    if (entries[0].boundingClientRect.y < -entries[0].target.clientHeight) {
+      document.body.classList.add("header-not-at-top");
+    } else {
+      document.body.classList.remove("header-not-at-top");
+    }
+  });
+  observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
+}
+
+setInterval(function () {
+  const collection = document.getElementsByClassName("stepsBlock_item");
+  for (i = 0; i < collection.length; i++) {
+    if (collection[i].classList.contains("stepsBlock_active")) {
+      collection[i].classList.remove("stepsBlock_active");
+      if (i == collection.length - 1) {
+        collection[0].classList.add("stepsBlock_active");
+      } else {
+        collection[i + 1].classList.add("stepsBlock_active");
+      }
+      return;
+    }
+  }
+}, 8000);
